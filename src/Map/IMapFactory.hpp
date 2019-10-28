c688fc4244ebf0b412ae051d4072e7e3b8238cd7
Stanislaw Babkin <stalissss@outlook.com>
2019-10-28 08:42:26 +0300

    Fix mines number, some guards

--- src/Map/IMapFactory.hpp
+++ src/Map/IMapFactory.hpp
@@ -7,21 +7,15 @@
 #include "IMap.hpp"
 #include <memory>
 
-static const int RANDOM_MAX = 0x7fffffff;
-
 static const int DEFAULT_WIDTH = 10;
 static const int DEFAULT_HEIGHT = 10;
-
-static const int DEFAULT_RANDOM_WINDOW = static_cast<int>(static_cast<float>(RANDOM_MAX) * 0.75);
 
 class IMapFactory {
 public:
     IMapFactory() = delete;
     IMapFactory(const IMapFactory&) = delete;
     static std::shared_ptr<IMap> CreateMap(int mineCount);
-    static std::shared_ptr<IMap> CreateMap(int mineCount, int randWindow);
     static std::shared_ptr<IMap> CreateMap(int mineCount, int width, int height);
-    static std::shared_ptr<IMap> CreateMap(int mineCount, int randWindow, int width, int height);
 };
 
 
